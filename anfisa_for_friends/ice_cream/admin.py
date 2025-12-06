a4e115f2f3d38627f9871a7441d3f73a9cf1ae96
dentoro299 <dentoro299@yandex.ru>
2025-12-06 15:42:41 +0300

    перед изменением в шаблоне

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Category, IceCream
+
 
 # Register your models here.
+admin.site.register(Category)
+
+
+class IceCreamAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'description',
+        'is_published',
+        'is_on_main',
+        'category',
+        'wrapper'
+    )
+    list_editable = (
+        'is_published',
+        'is_on_main',
+        'category'
+    )    
+    search_fields = ('title',) 
+    list_filter = ('category',)
+    list_display_links = ('title',)
+    
+admin.site.register(IceCream, IceCreamAdmin)
